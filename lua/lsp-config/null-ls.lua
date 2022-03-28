56aab8ec0cd0bc1773806b39e6ba6ba330106e4d
Perizote <sergio.perizote@gmail.com>
2022-03-28 15:17:55 +0200

    [Removed] black formatting onsave

--- lua/lsp-config/null-ls.lua
+++ lua/lsp-config/null-ls.lua
@@ -13,7 +13,7 @@ null_ls.setup({
     end,
     sources = {
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.black,
+        -- null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.eslint,
